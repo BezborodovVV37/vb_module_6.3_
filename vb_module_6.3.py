1818b3f2240ee27049fcf9fa6ac8ac7edcda6187
BezborodovVV37 <blood371993@gmail.com>
2024-09-17 14:03:17 +0300

    Add files via upload
    Задача "Мифическое наследование":

--- vb_module_6.3.py
+++ vb_module_6.3.py
@@ -1,40 +1,39 @@
-class Horse:  # класс описывающий лошадь.
-    x_distance = 0  # пройденный путь.
-    _sound = 'Frrr'  # звук, который издаёт лошадь.
+class Horse:
 
-    def run(self, dx):  # dx - изменение дистанции, увеличивает x_distance на dx
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
+
+    def run(self, dx):
         self.x_distance += dx
 
 
-class Eagle:  # класс описывающий орла
-    y_distance = 0  # высота полёта
-    sound = 'I train, eat, sleep, and repeat'  # звук, который издаёт орёл (отсылка)
+class Eagle:
 
-    def fly(self, dy):  # где dy - изменение дистанции, увеличивает y_distance на dy.
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep and repeat'
+
+    def fly(self, dy):
         self.y_distance += dy
 
 
-class Pegasus(Horse, Eagle):  # класс описывающий пегаса. Наследуется от Horse и Eagle в том же порядке.
-    # Объект такого класса должен обладать атрибутами классов родителей в порядке наследования.
-    # Также обладает методами:
-
+class Pegasus(Horse, Eagle):
     def __init__(self):
-        self.sound = super()._sound
-        self.sound = super().sound
+        super().__init__()
 
-    def move(self, dx, dy):  # - где dx и dy изменения дистанции.
-        self.run(dx)
-        self.fly(dy)
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
 
-    def get_pos(self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке
-        pos_pegasus = (self.x_distance, self.y_distance)
-        return pos_pegasus
+    def get_pos(self):
+        return self.x_distance, self.y_distance
 
-    def voice(self):  # печатает значение унаследованного атрибута sound.
+    def voice(self):
         print(self.sound)
 
 
-# print(Pegasus.mro())
 p1 = Pegasus()
 
 print(p1.get_pos())
